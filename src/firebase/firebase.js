df90b9a98ccfb57ed09261a3e01e1b57c708910f
realkewal <kew4l@yahoo.com.au>
2018-02-15 20:23:10 +1100

    Add private routes

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,4 @@
 import * as firebase from 'firebase';
-import moment from 'moment';
 
 const config = {
     apiKey: process.env.FIREBASE_API_KEY,
@@ -13,5 +12,6 @@ const config = {
 firebase.initializeApp(config);
 
 const database = firebase.database();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
-export { firebase, database as default };
+export { firebase, googleAuthProvider, database as default };
